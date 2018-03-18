4647446160318ed06fbdce432fa02120b7f0601d
Maciej <maciej.czarnota@gmail.com>
2018-03-19 02:03:01 +1000

    asasas

--- ng5/src/app/slide-show-with-search-bar/slide-show-with-search-bar.component.ts
+++ ng5/src/app/slide-show-with-search-bar/slide-show-with-search-bar.component.ts
@@ -1,8 +1,17 @@
-import { Component, OnInit} from '@angular/core';
-import {Observable} from 'rxjs/Observable';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  Observable
+} from 'rxjs/Observable';
 import 'rxjs/add/observable/interval';
-import {MatDialogRef} from '@angular/material';
-import {MAT_DIALOG_DATA} from '@angular/material';
+import {
+  MatDialogRef
+} from '@angular/material';
+import {
+  MAT_DIALOG_DATA
+} from '@angular/material';
 @Component({
   selector: 'app-slide-show-with-search-bar',
   templateUrl: './slide-show-with-search-bar.component.html',
@@ -15,13 +24,14 @@ export class SlideShowWithSearchBarComponent implements OnInit {
   counter = 0;
   active = false;
   searchContent = [
-    'Try "Bands in Brisbane"', 'Try "Jazz"', 'Try "Your favorite restaurant in area"', 'Try "Events in Sydney"' ];
-  pathsArray = [ 'band2.jpg', 'band1.jpg', 'band3.jpg', 'band4.jpg'];
+    'Try "Bands in Brisbane"', 'Try "Jazz"', 'Try "Your favorite restaurant in area"', 'Try "Events in Sydney"'
+  ];
+  pathsArray = ['band2.jpg', 'band1.jpg', 'band3.jpg', 'band4.jpg'];
   changePhoto() {
     // Change the photo of the slideshow to the next one. Resets if it
     // exceed the array length.
     console.log(this.counter);
-    this.counter ++;
+    this.counter++;
     if (this.counter >= this.pathsArray.length) {
       this.counter = 0;
     }
--- ng5/src/app/submission-form-next-step/submission-form-next-step.component.ts
+++ ng5/src/app/submission-form-next-step/submission-form-next-step.component.ts
@@ -1,10 +1,33 @@
-import { Component, OnInit, Output, EventEmitter, Input} from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatSelectModule} from '@angular/material/';
-import {MatIconModule} from '@angular/material/';
-import {FormControl, Validators, FormGroup, FormBuilder, FormGroupDirective, NgForm} from '@angular/forms';
-import {ErrorStateMatcher} from '@angular/material/core';
+import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter,
+  Input
+} from '@angular/core';
+import {
+  BrowserAnimationsModule
+} from '@angular/platform-browser/animations';
+import {
+  MatFormFieldModule
+} from '@angular/material/form-field';
+import {
+  MatSelectModule
+} from '@angular/material/';
+import {
+  MatIconModule
+} from '@angular/material/';
+import {
+  FormControl,
+  Validators,
+  FormGroup,
+  FormBuilder,
+  FormGroupDirective,
+  NgForm
+} from '@angular/forms';
+import {
+  ErrorStateMatcher
+} from '@angular/material/core';
 
 // /** Error when invalid control is dirty, touched, or submitted. */
 export class MyErrorStateMatcherComponent implements ErrorStateMatcher {
@@ -22,14 +45,13 @@ export class SubmissionFormNextStepComponent implements OnInit {
   matcher = new MyErrorStateMatcherComponent();
   public account = {
     password: null
-};
+  };
   public myColors = ['#DD2C00', '#FF6D00', '#FFD600', '#AEEA00', '#00C853'];
   // group of form controls
   public registrationForm: FormGroup;
   // checks for pattern => at least 1 letter, 1 number, 1 special
   private readonly emailRegex = '(?=\\D*\\d)(?=.*?[a-zA-Z]).*[\\W_].*';
   trigger = false;
-  passwordStrenght = ['weak', 'good', 'strong'];
   @Input() accountType;
   @Output() public submitSecondStepEvent = new EventEmitter();
   closeSecondSubmissionForm() {
@@ -48,17 +70,17 @@ export class SubmissionFormNextStepComponent implements OnInit {
     return control.hasError(error);
   }
   passwordMatchesEmail(passwordInput, emailInput) {
-  // compare email input value with password input value.
-  // return true if they are matched, false otherwise
-  return (passwordInput === emailInput) ? true : false;
+    // compare email input value with password input value.
+    // return true if they are matched, false otherwise
+    return (passwordInput === emailInput) ? true : false;
   }
+
   constructor(private readonly formBuilder: FormBuilder) {
     this.registrationForm = formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(30), Validators.pattern(this.emailRegex)]]
     });
-   }
-  ngOnInit() {
   }
+  ngOnInit() {}
 
 }
