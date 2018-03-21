1b434fa388dda1c9c78dc7d20e6c6ce84e557078
alexfiftysix <amiller5656@gmail.com>
2018-03-21 12:31:16 +1000

    Set up routing for main page. Default band profile card profile

--- ng5/src/app/app-routing.module.ts
+++ ng5/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {AboutComponent} from './about/about.component';
 import {ExtraComponent} from './extra/extra.component';
 import {SlideShowWithSearchBarComponent} from "./slide-show-with-search-bar/slide-show-with-search-bar.component";
 import {ProfileComponent} from "./profile/profile.component";
