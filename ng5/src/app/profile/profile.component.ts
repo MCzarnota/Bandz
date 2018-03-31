2c75768f9149f9f2dd0283bb2429fe551db69daa
alexfiftysix <amiller5656@gmail.com>
2018-03-31 13:17:33 +1000

    Simple dropdown added for rating bands. Doesn't work yet

--- ng5/src/app/profile/profile.component.ts
+++ ng5/src/app/profile/profile.component.ts
@@ -10,7 +10,7 @@ export class ProfileComponent implements OnInit {
   isBand = true;
   isVenue = false;
   title = 'Street Pieces';
-  banner = 'cover url("../../assets/images/band.jpg") no-repeat center';
+  banner = 'url("../../assets/images/band.jpg") no-repeat center';
   bio = 'Street Pieces formed in 2012 and soon began playing across their home state. After cutting their teeth on the live circuit they ' +
     'released their first EP featuring ‘Run’, earning them a support slot for Wolfmother and the title of ‘best band in Brisbane’ from' +
     ' Ugly Phil of Triple M. Coming off the success of their first single and a string of blistering live shows Street Pieces entered the' +
@@ -20,6 +20,12 @@ export class ProfileComponent implements OnInit {
     ' with an unforgettable show as they officially released \'Everything You Ever Wanted\' at The Foundry.\n' + '\n' + 'Listen below...';
   location = 'Brisbane';
   genre = 'Rock';
+  rateDropDown = false;
+
+  rate() {
+    // console.log('Rate');
+    this.rateDropDown = !this.rateDropDown;
+  }
 
   constructor() {
   }
